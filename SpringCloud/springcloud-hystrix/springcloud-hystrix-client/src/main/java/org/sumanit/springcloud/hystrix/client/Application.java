48e611d44eb8b4e899f0b342a7c5622dcb3de294
673565949@qq.com <suman35532859>
2019-04-22 08:32:50 +0800

    dd

--- SpringCloud/springcloud-hystrix/springcloud-hystrix-client/src/main/java/org/sumanit/springcloud/hystrix/client/Application.java
+++ SpringCloud/springcloud-hystrix/springcloud-hystrix-client/src/main/java/org/sumanit/springcloud/hystrix/client/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class Application {
     public static void main(String[] args) {
         try {
