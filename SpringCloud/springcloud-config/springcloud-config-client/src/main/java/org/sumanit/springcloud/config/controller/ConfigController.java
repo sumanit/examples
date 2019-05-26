bfc9e3c098b46f49ff1e4a857a06f867190219b9
673565949@qq.com <673565949@qq.com>
2019-05-26 23:37:12 +0800

    dd

--- SpringCloud/springcloud-config/springcloud-config-client/src/main/java/org/sumanit/springcloud/config/controller/ConfigController.java
+++ SpringCloud/springcloud-config/springcloud-config-client/src/main/java/org/sumanit/springcloud/config/controller/ConfigController.java
@@ -1,10 +1,12 @@
 package org.sumanit.springcloud.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
     @Value("${info.name:none}")
