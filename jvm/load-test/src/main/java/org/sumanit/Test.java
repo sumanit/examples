af025b40ada1b9fb8f1e1403eeb29584d4f04d5f
sum <673565949@qq.com>
2019-12-09 09:38:49 +0800

    dd

--- jvm/load-test/src/main/java/org/sumanit/Test.java
+++ jvm/load-test/src/main/java/org/sumanit/Test.java
@@ -5,22 +5,5 @@ package org.sumanit;
  */
 public class Test {
 
-    private static int value1= 0;
-
-    public static int value3=2;
-    private int value2 = 4;
-    static{
-        value1=4;
-        value3=3;
-    }
-    {
-        value3=5;
-        value2=6;
-    }
-
-
-    public static void main(String[] args) {
-        Test t = new Test();
-        System.out.println(value3);
-    }
+    private int value = 1;
 }
